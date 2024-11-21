80b835b926c3c65e8ee83f0bacf9831847d01855
albertogonzalez <almagohe38@gmail.com>
2024-11-21 16:21:45 -0600

    estilos y mejoras a contact

--- src/paginas/contacto.jsx
+++ src/paginas/contacto.jsx
@@ -1,39 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/img/logo.png";
+import "./contacto.css";
+import ImageSlider from "../componentes/imagenes";
 
 function Contacto() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    nombre: "",
+    email: "",
+    mensaje: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
+  const navigateHome = () => {
+    navigate("/");
+  };
+
+  const navigateToProyectos = () => {
+    navigate("/proyectos");
+  };
+
+  const navigateToServicios = () => {
+    navigate("/servicios");
+  };
+
   return (
-    <>
-      <form name="contact" method="post" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" required />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" required />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" required></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </>
+    <div className="todo">
+      {/* Header */}
+      <div className="parte-arriba">
+        <div className="logo">
+          <img src={logo} alt="Logo" className="milogo" />
+        </div>
+        <div className="botones">
+          <div className="btncontactocajah">
+            <button className="btncontacto" onClick={navigateHome}>
+              Home
+            </button>
+          </div>
+          <div className="btnservicioscajah">
+            <button className="btnservicio" onClick={navigateToServicios}>
+              Servicios
+            </button>
+          </div>
+          <div className="btnproyectoscajah">
+            <button className="btnproyectos" onClick={navigateToProyectos}>
+              Proyectos
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Main content */}
+      <div className="centro">
+        <div className="comentarios">
+          <div className="nombre">
+            <h1>Contact Us</h1>
+          </div>
+          <div className="lema">
+            <h1>"Estamos aquí para ayudarte."</h1>
+          </div>
+        </div>
+
+        {/* Formulario de contacto */}
+        <form
+          name="contacto" // tengo que asegurarme de que este nombre coincida con el que está configurado en Netlify
+          method="POST"
+          data-netlify="true"
+          onSubmit={handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contacto" />
+
+          <div>
+            <label>Nombre:</label>
+            <input
+              type="text"
+              name="nombre"
+              value={formData.nombre}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label>Mensaje:</label>
+            <textarea
+              name="mensaje"
+              value={formData.mensaje}
+              onChange={handleChange}
+            />
+          </div>
+          <button className="send" type="submit">
+            Enviar
+          </button>
+        </form>
+
+        <button className="btnproyectos" onClick={navigateToProyectos}>
+          Ver Proyectos
+        </button>
+      </div>
+
+      {/* Footer */}
+      <div className="footer">
+        <div className="logofooter">
+          <img src={logo} alt="Logo" className="milogofooter" />
+        </div>
+      </div>
+    </div>
   );
 }
 
