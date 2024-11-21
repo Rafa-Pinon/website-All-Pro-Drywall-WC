67566b1419ff70f68766803553f9da9056345d06
albertogonzalez <almagohe38@gmail.com>
2024-11-21 13:53:26 -0600

    formulario

--- src/paginas/contacto.jsx
+++ src/paginas/contacto.jsx
@@ -2,9 +2,38 @@ import React from "react";
 
 function Contacto() {
   return (
-    <div className="contacto">
-      <h1>hola soy contacto</h1>
-    </div>
+    <>
+      <form name="contact" method="post" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" required />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" required />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" required></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </>
   );
 }
 
