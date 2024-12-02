4da3bd9749385423e1a96821e0613e691d8e7207
albertogonzalez <almagohe38@gmail.com>
2024-12-02 15:25:03 -0600

    responsive header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/img/logo.png";
 import "../styles/header.css";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
 
   const navLinks = [
@@ -14,19 +15,33 @@ function Header() {
     { path: "/proyectos", label: "Projects" },
   ];
 
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <header className="header">
       <div className="logo">
         <img src={logo} alt="All-Pro Drywall Logo" />
       </div>
-      <nav className="nav">
-        {navLinks.map((link) =>
-          location.pathname === link.path ? null : (
-            <Link key={link.path} to={link.path}>
+      <div
+        className={`hamburger ${isMenuOpen ? "active" : ""}`}
+        onClick={toggleMenu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={`nav ${isMenuOpen ? "active" : ""}`}>
+        {navLinks
+          .filter((link) => link.path !== location.pathname)
+          .map((link) => (
+            <Link
+              key={link.path}
+              to={link.path}
+              onClick={() => setIsMenuOpen(false)}
+            >
               {link.label}
             </Link>
-          )
-        )}
+          ))}
       </nav>
     </header>
   );
