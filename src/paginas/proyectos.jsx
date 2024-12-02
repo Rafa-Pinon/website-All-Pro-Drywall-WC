7c9f1be203ded1750fdb0c76fab5cbdc85559967
Rafa-Pinon <rafapgtodo@gmail.com>
2024-12-02 16:07:45 -0600

    modificacion de header estilos cel

--- src/paginas/proyectos.jsx
+++ src/paginas/proyectos.jsx
@@ -4,10 +4,17 @@ import Header from "../componentes/Header";
 import uno from "../../src/assets/imagenes/uno.jpg";
 import dos from "../../src/assets/imagenes/dos.jpg";
 import tres from "../../src/assets/imagenes/tres.jpg";
+import cuatro from "../../src/assets/imagenes/cuatro.jpg";
+import cinco from "../../src/assets/imagenes/cinco.jpg";
+import seis from "../../src/assets/imagenes/seis.jpg";
+import texture from "../../src/assets/imagenes/texture.jpg";
+import tab from "../../src/assets/imagenes/tab.jpeg";
+import ml from "../../src/assets/imagenes/ml.jpeg";
 import ImagenesProyectosSlider from "../componentes/imagenesproyectoslider";
 
 function Proyectos() {
-  const images = [uno, dos, tres]; // Lista de imágenes
+  const imagenes = [uno, dos, tres]; // Lista de imágenes
+  const imagenesdos = [texture, seis, tab, ml]; // Lista de imágenes
   return (
     <div className="proyecto">
       <div className="cabeza">
@@ -18,7 +25,19 @@ function Proyectos() {
       </div>
 
       <div className="fotos">
-        <ImagenesProyectosSlider images={images} />
+        <div className="frase">
+          <p>"The beginning is the first brick toward your future"</p>
+        </div>
+        <ImagenesProyectosSlider imagenes={imagenes} />
+      </div>
+      <div className="temaslider">
+        <p>Interiores</p>
+      </div>
+      <div className="sliderdos">
+        <ImagenesProyectosSlider imagenes={imagenesdos} />
+      </div>
+      <div className="foter">
+        <h1>foter</h1>
       </div>
     </div>
   );
